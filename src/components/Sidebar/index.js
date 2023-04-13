6ef1ba0c5a2da39c7783457be3fea85ebf78895f
Bradley Kimmance <bradleykimmance@gmail.com>
2023-04-14 00:38:25 +0100

    Add Sidebar and Homepage Background

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,42 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+// import LogoB from '../../assets/images/logo-b.png'
+// import LogoLower from '../../assets/images/logo-lower.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
+
+const Sidebar = () => (
+  <div className='nav-bar'>
+    <Link className='logo' to='/'>
+      {/*<img src={LogoB} alt="logo" />*/}
+      {/*<img className="sub-logo" src={LogoLower} alt="Bradley Kimmance" />*/}
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/bradleykimmance/'>
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://github.com/bradleykimmance'>
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
