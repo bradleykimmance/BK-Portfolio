d572953de696b3142839cd41feaafca013baa65b
Bradley Kimmance <bradleykimmance@gmail.com>
2023-04-13 23:13:30 +0100

    Added Fonts and basic page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.scss';
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/layout'
 
 function App() {
   return (
-      null
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+      </>
       )
 }
 
