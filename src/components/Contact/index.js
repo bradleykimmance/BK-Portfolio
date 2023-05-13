b15a4af4eecb870f5cf93ea5e9b933569dec2704
Bradley Kimmance <bradleykimmance@gmail.com>
2023-05-13 12:59:46 +0100

    Website ready for personal information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,14 @@
 import './index.scss';
 import Loader from 'react-loaders'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import emailJS from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
-
   const [letterClass, setLetterClass] = useState('text-animate')
   const contactArray = "Contact me".split("")
+  const refForm = useRef()
 
   useEffect(() => {
     let timeoutId = setTimeout(() => {
@@ -16,6 +18,27 @@ const Contact = () => {
       clearTimeout(timeoutId)
     }
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailJS
+      .sendForm(
+        'service_4n3f7a9',
+        'template_pwvkk3g',
+        refForm.current,
+        'M5MIU9AkaeMBQ-7H-'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -35,13 +58,13 @@ const Contact = () => {
             Malesuada proin libero nunc consequat interdum.
           </p>
           <div className='contact-form'>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className='half'>
-                  <input type="text" name="name" placeholder="Name" required />
+                  <input type="text" name="user_name" placeholder="Name" required />
                 </li>
                 <li className='half'>
-                  <input type="email" name="email" placeholder="Email Address" required />
+                  <input type="email" name="user_email" placeholder="Email Address" required />
                 </li>
                 <li>
                   <input type="text" name="subject" placeholder="Subject" required />
@@ -55,6 +78,27 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Bradley Kimmance,
+          <br />
+          England, United Kingdom,
+          <br />
+          Bedfordshire, <br />
+          Stotfold <br />
+          <span>bradleykimmance@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[52.0162, -0.23218]} zoom={13}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[52.0162, -0.23218]}>
+              <Popup>
+                Bradley Kimmance lives here
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="ball-pulse" />
